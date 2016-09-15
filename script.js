86e6090695232ba92af1b0283438e357e8d1fdc5
Aleksandr Sadykov <sadykov-av@fxclub.org>
2016-09-15 11:35:46 +0200

    Sync files

---
+++ script.js
@@ -0,0 +1,7 @@
+var getByClass = className => document.querySelectorAll(className);
+
+// func показать n слайд
+// init нарисовать пупочки
+
+// init нарисовать стрелки
+// init автоплей, отключаемый по навигации
